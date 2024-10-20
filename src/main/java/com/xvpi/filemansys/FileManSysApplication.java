0c837f3cba08fa492bbfc670e103f2d1cd76d17f
xvpi <904017172@qq.com>
2024-10-20 14:48:29 +0800

    20241020-x-1-init

---
+++ src/main/java/com/xvpi/filemansys/FileManSysApplication.java
@@ -0,0 +1,13 @@
+package com.xvpi.filemansys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManSysApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello java!");
+    }
+
+}
---
+++ src/test/java/com/xvpi/filemansys/FileManSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xvpi.filemansys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
